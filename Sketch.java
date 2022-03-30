20fd8967667d6685a20e5fac9169f89ea5ee7be2
Ryan Shiu <75390887+Ryan-Shiu@users.noreply.github.com>
2022-03-30 03:51:00 +0000

    Completed all 4 quadrants

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,54 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+/**
+ * A program Sketch.java that draws 4 quadrants with different drawings identically to the sample
+ * @author: R.Shiu
+ */
+  
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(255, 255, 255);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+  // Drawing the 10 by 10 grid (top left quadrant)
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    for(int lineX = width/2; lineX > 0; lineX -= 20){
+     line(width/2 , lineX, 0, lineX);
+     line(lineX, height/2, lineX, 0);
+   }
+    
+  // Drawing the 5 x 5 purple circles (top right quadrant)
+    for(int circleX = 220; circleX <= 380; circleX += 40){
+      for(int circleY = 20; circleY < 220; circleY += 40){
+        fill(204, 0, 204);
+        ellipse(circleX, circleY, 20, 20);
+      }
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+  // Drawing the gradient (bottom left quadrant)
+    for(int gradient = 0; gradient <= width/2; gradient++){
+      stroke(gradient);
+      line(gradient, height/2, gradient, height);
+    }
+
+    // Drawing flower pedals (bottom right quadrant)
+    stroke(0);
+    translate((float) (width * 0.75),(float) (height * 0.75));
+    for(int pedal = 1; pedal <= 9; pedal += 1){
+      fill(255, 128, 0);
+      ellipse(0, 0, 20, 130);
+      rotate(radians(45));
+    }
+    // Drawing the middle circle for the flower (bottom right quadrant)
+    fill(0, 153, 0);
+    ellipse(0, 0, 30, 30);
+
+}
+
   
   // define other methods down here.
 }
